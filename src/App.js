66bebb237d43f87b3a9de4616c1595d13ea28d7f
hannahmnixon <90346288+hannahmnixon@users.noreply.github.com>
2021-11-16 14:43:28 -0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Application</h1>
-      <footer>
-        This project was created by Hannah Nixon and is{" "}
-        <a
-          href="https://github.com/hannahmnixon/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather Application</h1>
+        <footer>
+          This project was created by Hannah Nixon and is{" "}
+          <a
+            href="https://github.com/hannahmnixon/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
