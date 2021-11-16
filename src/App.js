2f3665d8612e281292f9b9dc2cda71a9c2dd8067
hannahmnixon <90346288+hannahmnixon@users.noreply.github.com>
2021-11-16 14:21:51 -0800

    added github repo link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      <h1>Weather Application</h1>
+      <footer>
+        This project was created by Hannah Nixon and is{" "}
+        <a
+          href="https://github.com/hannahmnixon/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
