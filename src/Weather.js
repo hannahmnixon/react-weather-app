66a8c06c2ef6d4c85fa3f08b38a7296d17b40b5f
hannahmnixon <90346288+hannahmnixon@users.noreply.github.com>
2021-11-16 17:02:19 -0800

    added useStates

--- src/Weather.js
+++ src/Weather.js
@@ -3,48 +3,68 @@ import "./App.css";
 import axios from "axios";
 
 export default function Weather() {
-  const [temperature, setTemperature] = useState(null);
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
 
-  const apiKey = "a6ff4b7a9b12bc3ba444e702f94356c3";
-  let city = "San Francisco";
-  let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row justify-content-end">
-          <div className="col-5">
-            <input
-              type="search"
-              placeholder="see another city"
-              className="form-control"
-              autoFocus="on"
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row justify-content-end">
+            <div className="col-5">
+              <input
+                type="search"
+                placeholder="see another city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+          </div>
+          <div className="col-4">Right now in...</div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="description">{weatherData.description}</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Partly cloudy"
             />
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="units">°F</span>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
           </div>
         </div>
-        <div className="col-4">Right now in...</div>
-      </form>
-      <h1>San Francisco</h1>
-      <ul>
-        <li>Tuesday, November 16 2:25pm</li>
-        <li className="description">Partly cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Partly cloudy"
-          />
-          <span className="temperature">16</span>
-          <span className="units">°F</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 2%</li>
-            <li>Humidity: 77%</li>
-            <li>Wind: 14 km/h</li>
-          </ul>
-        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "a6ff4b7a9b12bc3ba444e702f94356c3";
+    let city = "San Francisco";
+    let apiUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "loading...";
+  }
 }
